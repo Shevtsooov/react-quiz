d523071fe02ddd3316dc135b492475c77489bf17
Igor Shevtsov <contact.shevtsov@gmail.com>
2023-07-02 21:13:26 +0300

    created the quiz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.scss';
+import React, { useState } from 'react';
 
-function App() {
+const questions = [
+  {
+    title: 'React - это ... ?',
+    answers: ['библиотека', 'фреймворк', 'приложение'],
+    correct: 0,
+  },
+  {
+    title: 'Компонент - это ... ',
+    answers: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    correct: 1,
+  },
+  {
+    title: 'Что такое JSX?',
+    answers: [
+      'Это простой HTML',
+      'Это функция',
+      'Это тот же HTML, но с возможностью выполнять JS-код',
+    ],
+    correct: 2,
+  },
+];
+
+function Result({ correct }) {
+  const correctForm = correct < 5
+    ? 'питання'
+    : 'иитань'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="result">
+      <img
+      src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"
+      alt="Congratulations"
+      />
+      <h2>
+        {`Ви правильно відповіли на ${correct} ${correctForm} з ${questions.length}`}</h2>
+      <a href="/">
+        <button>Спробувати знову</button>
+      </a>
     </div>
   );
 }
 
-export default App;
+function Game({ question, step, onAnswerClick }) {
+  const percentage = Math.round(step / questions.length * 100);
+
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width:`${percentage}%` }} className="progress__inner"></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.answers.map((answer, index) => (
+          <li
+            key={answer}
+            onClick={() => onAnswerClick(index)}
+          >
+            {answer}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export const App = () => {
+  const [step, setStep] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  
+  const question = questions[step];
+
+  const onAnswerClick = (index) => {
+    setStep(step + 1);
+
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+    }
+  }
+
+  return (
+    <div className="App">
+      {step !== questions.length
+      ? (
+        <Game
+          question={question}
+          step={step}
+          onAnswerClick={onAnswerClick}
+        />
+      )
+      : <Result correct={correct} />}
+    </div>
+  );
+}
+
